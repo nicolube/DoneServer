bb14b418137be480f84895e66d8ef1a951e8cb56
Nico <dev@nicolube.de>
2022-01-28 07:23:41 +0100

    Adding an prototype for github file explorer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Map, {DroneMap} from "./components/maps";
+import Map, { DroneMap } from "./components/maps";
 import { Container, Ratio } from "react-bootstrap"
+import { GitHubFiles } from './components/test';
 
 
 function App() {
@@ -10,11 +11,11 @@ function App() {
       </header>
       Hallo Welt
       <Container className="w-100 h-100" >
-        <Ratio  aspectRatio="4x3">
-          <DroneMap/>
+        <Ratio aspectRatio="4x3">
+          <DroneMap />
         </Ratio>
+        <GitHubFiles className="mx-auto" />
       </Container>
-
     </div>
   );
 }
